37c36deffc3d6514b486514741bffd7ecf1ad153
Mark Szulc <mszulc@adobe.com>
2025-02-18 13:32:46 +1100

    Update AEM environment

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p137825-e1511252.adobeaemcloud.com';
+  return 'https://publish-p149891-e1546481.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p137825-e1511252.adobeaemcloud.com';
+  return 'https://author-p149891-e1546481.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
