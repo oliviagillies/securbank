20040d79da7b65606d661a8bdfd9b369a63251ae
Mark Szulc <mszulc@adobe.com>
2025-02-18 21:18:43 +1100

    Catch variation not set

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -6,7 +6,10 @@ export default async function decorate(block) {
   const aemauthorurl = getAEMAuthor();
   const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
   const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
-  const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
+  let variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
+  if (!variationname) {
+    variationname = 'main';
+  }
 
   const url = window.location && window.location.origin && window.location.origin.includes('author')
     ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
